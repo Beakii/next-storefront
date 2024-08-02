d08fa3406218f61e8c5e13d18aa930609993a5e8
James <jamestewartpersonal@gmail.com>
2024-08-03 11:16:47 +1200

    updated prisma schema to reflect payment handling database

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,20 @@
 import NextAuth from 'next-auth';
-import { authOptions } from './options';
+import GoogleProvider from "next-auth/providers/google";
 
-export const handler = NextAuth(authOptions);
+export const handler = NextAuth({
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID!,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+			authorization: {
+				params: {
+					prompt: "consent",
+					access_type: "offline",
+					response_type: "code"
+				}
+			}
+		})
+    ],
+});
 
 export { handler as GET, handler as POST }
--- src/app/configure/review/actions.ts
+++ src/app/configure/review/actions.ts
@@ -1,10 +1,25 @@
 "use server";
 
+import { getServerSession } from "next-auth";
+import { BASE_PRICE, PRODUCT_PRICES } from "~/app/config/products";
 import { getOneConfig } from "~/db/queries";
 
 export const createCheckoutSession = async ({configId}:{configId:string}) => {
 	const config = await getOneConfig({configId});
 	if (!config) {
 		throw new Error("Config not found");
+	}
+
+	const session = await getServerSession();
+	if(!session) {
+		throw new Error("You must be logged in to proceed");
+	}
+
+	let price = BASE_PRICE;
+	if(config.caseFinish === "textured") {
+		price += PRODUCT_PRICES.finish.textured;
+	}
+	if(config.caseMaterial === "polycarbonate") {
+		price += PRODUCT_PRICES.material.polycarbonate;
 	}
 }
